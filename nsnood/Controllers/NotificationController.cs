44fd9fe3d6005b36722940ae6e3ec68d04a9744e
sebastiaan1997 <sebastiaan.saarloos@gmail.com>
2018-11-23 14:40:15 +0100

    NotificationController

--- nsnood/Controllers/NotificationController.cs
+++ nsnood/Controllers/NotificationController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using nsnood.model;
+using nsnood.repo;
 
 namespace nsnood.Controllers
 {
@@ -12,6 +15,57 @@ namespace nsnood.Controllers
     [ApiController]
     public class NotificationController : ControllerBase
     {
+        private readonly NotificationRepo _notificationRepo;
+        public IEnumerable<Notification> Index()
+        {
+            return _notificationRepo.GetAll();
+        }
+
+        [Route("notificaties/{id}")]
+        public ActionResult GetNotification(string id)
+        {
+            if (Guid.TryParse(id, out Guid guid))
+            {
+                // TODO Check if exists
+                if (true)
+                {
+                    
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return this.BadRequest();
+        }
+
+        [Route("notificaties/treinstel/{id}")]
+        public ActionResult GetNotificationsByMaterial(int id)
+        {
+            if (id < 0)
+            {
+                return NotFound();
+            }
+
+            return BadRequest();
+        }
+
+        [Route("notificaties/medewerker/{id}")]
+        public ActionResult GetNotificationByWorker(int id)
+        {
+            if (id < 0)
+            {
+                return NotFound();
+            }
+
+            if (true)
+            {
+                
+            }
+            return BadRequest();
+        }
+        
+        
         [HttpPost("komtmeldingaan")]
         public ActionResult<string> KomtMeldingAan([FromBody] MeldingSoort meldingSoort)
         {
@@ -29,5 +83,8 @@ namespace nsnood.Controllers
         {
 
         }
+        
+        
+        
     }
 }
--- nsnood/repo/NotificationRepo.cs
+++ nsnood/repo/NotificationRepo.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using nsnood.model;
+
 namespace nsnood.repo
 {
     public class NotificationRepo
     {
-        
+        public IEnumerable<Notification> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
